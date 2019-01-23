c81d92831beffd4cac088fadf4a0888253f4bb5a
Matheus Rodrigues <matheusrf96@gmail.com>
2019-01-23 00:10:04 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/node-api',
+    { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3000);
